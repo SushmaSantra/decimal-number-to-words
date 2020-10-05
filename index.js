c81c1984facd1fa0efcc546ac4bcfcf184a0ed44
sushmasantra <sushmasantra@gmail.com>
2020-10-05 17:39:05 +0530

    toWords function

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 var converter = require('number-to-words');
-converter.toOrdinal(21); // => “21st”
-
-console.log(converter.toOrdinal(21));
+export default {
+    toWords: function(n){
+        if (!n) return ''
+        if((/[^0-9.]/g).test(n)) return ''
+        if(n <= 0) return ''
+    
+        var nums = n.toString().split('.')
+    
+        if(nums.length && nums[0] !== undefined )
+        {
+            var whole = converter.toWords(nums[0])
+    
+            if (nums.length == 2 && nums[1] !== '') {
+                var fractionstr = nums[1].toString();
+                var fraction = '';
+                for(var i=0;i<fractionstr.length;i++){
+                    fraction = fraction+' ' + converter.toWords(fractionstr.charAt(i));
+                }
+                
+                return whole + ' point ' + fraction;
+            } else {
+                return whole;
+            }
+        }
+    }
+  }
